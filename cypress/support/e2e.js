700dcb124f5c26f06380427cde2a66b7af1ed6b3
tqbrandao <tqbrandao@gmail.com>
2023-01-18 16:35:31 -0300

    Fixed XHR logs

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -23,3 +23,13 @@ require("cypress-plugin-tab");
 Cypress.on("uncaught:exception", (err, runnable) => {
   return false;
 });
+
+const app = window.top;
+if (!app.document.head.querySelector("[data-hide-command-log-request]")) {
+  const style = app.document.createElement("style");
+  style.innerHTML =
+    ".command-name-request, .command-name-xhr { display: none }";
+  style.setAttribute("data-hide-command-log-request", "");
+
+  app.document.head.appendChild(style);
+}
